70fdc5cc8040ccca55e6e8082233771faaf0a506
Reginald Osano <regie30developer@gmail.com>
2022-09-18 07:01:23 +0800

    Refactoring of codes to make it compliant to be a supergraph gateway

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,46 @@
-console.log("Hello Gateway!!!")
+import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
+import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from  "apollo-server-core";
+import dotenv from "dotenv-safe";
+
+dotenv.config();
+
+const startServer = () => {
+	const gateWay = new ApolloGateway({
+		supergraphSdl: new IntrospectAndCompose({
+			subgraphs:  [
+				{
+					name: "users",
+					url: process.env.USERS_SERVICE_URL,
+				},
+				
+			]
+		})
+	});
+
+  const server = new ApolloServer({
+		gateway: gateWay,
+		cors: {
+	  	origin: "*",
+	  	credentials: true,
+	  },
+		csrfPrevention: true, 
+  	introspection: true,
+	  plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false, embed: true })] ,
+	});
+
+	 // Start the server
+	 server
+	  .listen(process.env.PORT || 4000)
+	  .then(({ url }) => {
+		  	 // eslint-disable-next-line no-console
+		  	console.log(`'🚀 Server ready @ ${url}`);
+	  })
+	  .catch((error) => {
+		   // eslint-disable-next-line no-console
+		  	console.log(`There was an error - ${error}`);
+    });
+
+}
+
+startServer();
